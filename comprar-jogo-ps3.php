e5a45f0f82c1bae4d4e52108f078c5cdf399c1f6
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-01-14 11:22:50 -0300

    Update comprar-jogo-ps3.php

--- comprar-jogo-ps3.php
+++ comprar-jogo-ps3.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="icons/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=arrow_back" />
     <title>GuiJogosPs3</title>
 </head>
 <body style="text-align: center;">
     <h1>Aprovado</h1>
     <img src="https://upload.wikimedia.org/wikipedia/commons/8/84/Approved_img.png" alt="">
     <p>Jogo de ps3 comprado com sucesso</p>
+    <button><a href="index.php" style="text-decoration: none;"><span class="material-symbols-outlined">arrow_back</span></a></button>
 </body>
 </html>
