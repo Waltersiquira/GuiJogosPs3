e09b32a76c5c5a0b86afd9ad58f484ead7cb0eec
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-01-14 11:20:13 -0300

    Create comprar-jogo-ps3.php

---
+++ comprar-jogo-ps3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="icons/favicon.ico" type="image/x-icon">
+    <title>GuiJogosPs3</title>
+</head>
+<body style="text-align: center;">
+    <h1>Aprovado</h1>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/8/84/Approved_img.png" alt="">
+    <p>Jogo de ps3 comprado com sucesso</p>
+</body>
+</html>
