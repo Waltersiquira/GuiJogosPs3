2ea0ce27930574f246979c00c3f7f057c27d407e
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-01-14 11:00:04 -0300

    Create lojas.php

---
+++ includes/lojas.php
@@ -0,0 +1,9 @@
+<?php 
+$l = new mysqli('localhost', 'root', '', 'lojas');
+if ($l->connect_error){
+    die('erro de conexão co, banco de dados');
+}
+$l->query('set character_set_connection = utf8mb4');
+$l->query('set character_set_client = utf8mb4');
+$l->query('set character_set_results = utf8mb4');
+?>
