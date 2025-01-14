c4a39dd7a35ee4dc5809958bdd748a913d655a76
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-01-14 11:24:12 -0300

    Update pagina-jogo-ps3.php

--- pagina-jogo-ps3.php
+++ pagina-jogo-ps3.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="icons/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=arrow_back" />
     <title>GuiJogosPs3</title>
 </head>
 <body>
@@ -23,7 +24,8 @@
         }
     }
     ?>
-    <button style="background-color: blue;"><a href="comprar-jogo-ps3.php" style="text-decoration: none; color: white;">Comprar</a></button>
+    <button style="background-color: blue;"><a href="comprar-jogo-ps3.php" style="text-decoration: none; color: white;">Comprar</a></button> <br>
+    <button><a href="index.php"><span class="material-symbols-outlined">arrow_back</span></a></button>
     <?php $l->close() ?>
 </body>
 </html>
